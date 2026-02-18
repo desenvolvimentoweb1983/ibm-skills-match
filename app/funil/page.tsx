0b6c948cd7bfd2ff3e5cb58601b635081220fd62
luis oliveira <desenvolvimentoweb1983@gmail.com>
2026-02-18 18:09:43 -0300

    TESTE 2  deploy Vecel

---
+++ app/funil/page.tsx
@@ -0,0 +1,13 @@
+export default function Page() {
+  return (
+    <main className="max-w-5xl mx-auto px-6 py-16">
+      <h1 className="text-3xl font-bold mb-4">
+        Página em construção
+      </h1>
+
+      <p className="text-zinc-600">
+        O conteúdo será adicionado em breve.
+      </p>
+    </main>
+  );
+}
